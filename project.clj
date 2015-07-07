a2e553a03248badde0e4191c1a227db85e359338
Gergely Nagy <algernon@madhouse-project.org>
2015-07-07 08:41:44 +0200

    project.clj: Bump Clojure dependency to 1.7.0
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
             :url "http://creativecommons.org/licenses/by-sa/3.0/"
             :distribution :repo}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [enlive/enlive "1.1.1" :exclusions [org.clojure/clojure]]
                  [clj-time "0.4.4" :exclusions [org.clojure/clojure]]
                  [fs "1.3.2"]
