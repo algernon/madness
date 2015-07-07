06e88ddecb22f6d41b917ee940cb79d4f49e3848
Gergely Nagy <algernon@madhouse-project.org>
2015-07-07 08:44:11 +0200

    settings.clj: Switch to https:// URLs by default
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- settings.clj
+++ settings.clj
@@ -1,10 +1,10 @@
 {:template {:default "asylum7-main.html",
             :atom "asylum3-atom.xml"
-            :base-url "http://asylum.madhouse-project.org"},
+            :base-url "https://asylum.madhouse-project.org"},
  :recent-posts {:columns 1
                 :rows 2
                 :total 2}
  :archive-posts {:columns 1
                  :rows -1}
- :atom {:base-url "http://asylum.madhouse-project.org"
+ :atom {:base-url "https://asylum.madhouse-project.org"
         :title "Asylum Archives"}}
