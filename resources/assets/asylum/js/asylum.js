2d2314ef1b24d32e31784a42249b596d1ad56be5
Gergely Nagy <algernon@madhouse-project.org>
2018-04-24 06:03:33 +0200

    Drop the custom key bindings
    They are more annoying than what they're worth.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- resources/assets/asylum/js/asylum.js
+++ resources/assets/asylum/js/asylum.js
@@ -2,33 +2,4 @@ $(document).ready(function () {
   $("a[rel=tooltip]").tooltip();
   $(".rel-tooltip").tooltip();
   $(".row-fluid *[rel=tooltip]").tooltip();
-  $("a[href=#distro-debian-wheezy]").tab('show');
-
-  $(".madness-share-twitter").attr("href", "https://twitter.com/share?via=algernoone&url=" + encodeURIComponent(document.location.toString()) + "&text=" + encodeURIComponent(document.title));
-  $(".madness-share-google-plus").attr("href", "https://plus.google.com/share?url=" + encodeURIComponent(document.location.toString()));
-  $(".madness-share-flattr").attr
-    ("href",
-     "https://flattr.com/submit/auto?user_id=algernon&title=" + encodeURIComponent(document.title) + "&category=text&url=" + encodeURIComponent(document.location.toString()))
-
-  function load_page(o) {
-      var x = o.attr("href");
-      if (x) {
-          document.location = x;
-      }
-  }
-
-  Mousetrap.bind('j', function () {
-      load_page($(".next a"));
-  });
-
-  Mousetrap.bind('k', function () {
-      load_page($(".previous a"));
-  });
-
-  Mousetrap.bind('d', function () {
-      load_page($("#madness-article-date"));
-  });
-    /*
-  $(".madness-article-frame").focus();
-     */
 });
