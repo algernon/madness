1f1eb19ecba2807347fcb11731add903b38c1a34
Gergely Nagy <algernon@madhouse-project.org>
2018-09-04 00:23:33 +0200

    Implement "Add to Homescreen" support
    A bit of a work in progress, but it gets the job mostly done.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ resources/pwa/sw.js
@@ -0,0 +1,29 @@
+const version = "0.20180903";
+const cacheName = `asylum-${version}`;
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                `/`,
+                `/index.html`,
+                `/blog/archives/`,
+                `/assets/asylum/css/asylum8.min.css`
+            ]).then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+            .then(cache => cache.match(event.request, {ignoreSearch: true}))
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
